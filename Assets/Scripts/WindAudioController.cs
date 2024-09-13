0e235156dfd639c6ac18730cb2a4fec9159e1c20
CoCkMelon <rosgu69@inbox.ru>
2024-09-13 20:20:27 +0300

    Fix sound issues

--- Assets/Scripts/WindAudioController.cs
+++ Assets/Scripts/WindAudioController.cs
@@ -4,10 +4,15 @@ using UnityEngine.Audio;
 public class WindAudioController : MonoBehaviour
 {
     public WindForLayer windController;
-    public AudioMixerGroup calmMixerGroup;
-    public AudioMixerGroup stormMixerGroup;
+    public AudioMixerGroup calmMusicMixerGroup;
+    public AudioMixerGroup calmSoundMixerGroup;
+    public AudioMixerGroup stormMusicMixerGroup;
+    public AudioMixerGroup stormSoundMixerGroup;
+    public AudioMixerGroup mixer;
     public AudioSource calmMusicSource;
+    public AudioSource[] calmSoundSources;
     public AudioSource stormMusicSource;
+    public AudioSource[] stormSoundSources;
     public float calmThreshold = 1f;
     public float stormThreshold = 5f;
     public float transitionDuration = 2f;
@@ -33,15 +38,19 @@ public class WindAudioController : MonoBehaviour
     void UpdateAudioVolumes()
     {
         // Determine target volumes based on current wind force
-        targetVolumeCalm = currentWindForce < calmThreshold ? 1f : 0f;
-        targetVolumeStorm = currentWindForce >= stormThreshold ? 1f : 0f;
+        targetVolumeCalm = currentWindForce < calmThreshold ? 0f : 1f;
+        targetVolumeStorm = currentWindForce >= stormThreshold ? 0f : 1f;
 
         // Lerp towards target volumes
         volumeCalmLerpFactor = Mathf.Lerp(volumeCalmLerpFactor, targetVolumeCalm, transitionDuration * Time.deltaTime);
         volumeStormLerpFactor = Mathf.Lerp(volumeStormLerpFactor, targetVolumeStorm, transitionDuration * Time.deltaTime);
 
         // Apply volumes to audio mixer groups
-        calmMixerGroup.audioMixer.SetFloat("CalmVolume", volumeCalmLerpFactor);
-        stormMixerGroup.audioMixer.SetFloat("StormVolume", volumeStormLerpFactor);
+        mixer.audioMixer.SetFloat("CalmVolume", volumeCalmLerpFactor);
+        calmMusicMixerGroup.audioMixer.SetFloat("CalmVolume", volumeCalmLerpFactor*-1000);
+        calmSoundMixerGroup.audioMixer.SetFloat("CalmVolume2", volumeCalmLerpFactor*-1000);
+        mixer.audioMixer.SetFloat("StormVolume", volumeStormLerpFactor);
+        stormMusicMixerGroup.audioMixer.SetFloat("StormVolume", volumeStormLerpFactor*-1000);
+        stormSoundMixerGroup.audioMixer.SetFloat("StormVolume2", volumeStormLerpFactor*-1000);
     }
 }
